1baa6b7f7b0f64e4f979132c955df8ca3b4f07b2
Jörn Ott <joern.ott@ott-consult.de>
2022-04-27 17:01:09 +0200

    Fix confusion due to divergent dash/underscore handling

--- contrib/version.sh
+++ contrib/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# see https://github.com/joernott/monitoring-check_systemd-service/tags
+# see https://github.com/joernott/monitoring-check-systemd-service/tags
 APP="monitoring-check_systemd_service"
 VERSION="1.2.0"
 RELEASE="1"
-URL="https://github.com/joernott/monitoring-check_systemd_service/archive/refs/tags/v${VERSION}.tar.gz"
+URL="https://github.com/joernott/monitoring-check-systemd-service/archive/refs/tags/v${VERSION}.tar.gz"
 ARCH="noarch"
